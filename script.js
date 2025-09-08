7cd7cce54b4bf82e6b4cea36303d9c057f5839f1
EenSelembe <rendy.waniko@gmail.com>
2025-09-08 20:22:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const tiles = [
+  "img/tiles/dots-1.png", "img/tiles/dots-2.png", "img/tiles/dots-3.png",
+  "img/tiles/dots-4.png", "img/tiles/dots-5.png", "img/tiles/dots-6.png",
+  "img/tiles/dots-7.png", "img/tiles/dots-8.png", "img/tiles/dots-9.png",
+
+  "img/tiles/bamboo-1.png", "img/tiles/bamboo-2.png", "img/tiles/bamboo-3.png",
+  "img/tiles/bamboo-4.png", "img/tiles/bamboo-5.png", "img/tiles/bamboo-6.png",
+  "img/tiles/bamboo-7.png", "img/tiles/bamboo-8.png", "img/tiles/bamboo-9.png",
+
+  "img/tiles/characters-1.png", "img/tiles/characters-2.png", "img/tiles/characters-3.png",
+  "img/tiles/characters-4.png", "img/tiles/characters-5.png", "img/tiles/characters-6.png",
+  "img/tiles/characters-7.png", "img/tiles/characters-8.png", "img/tiles/characters-9.png",
+
+  "img/tiles/wind-east.png", "img/tiles/wind-south.png",
+  "img/tiles/wind-west.png", "img/tiles/wind-north.png",
+
+  "img/tiles/dragon-red.png", "img/tiles/dragon-green.png", "img/tiles/dragon-white.png",
+
+  "img/tiles/wild.png", "img/tiles/scatter.png"
+];
